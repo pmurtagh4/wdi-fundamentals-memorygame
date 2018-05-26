df8742f5f53c1942f062b86ccfd324941d8f1ea5
Philip Murtagh <philipmurtagh@Philips-Air.home>
2018-05-26 14:20:18 -0400

    cretaed logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,25 +3,31 @@
 var cards = ["queen", "queen", "king", "king"];
 //created another array to store the cards that are in play
 var cardsInPlay = [];
-//created a variable cardOne to display the first card user flipped
-var cardOne = cards[0];
-//added cardOne to array of cards that are in play
-cardsInPlay.push(cardOne);
-//used console.log() to display the card the user flipped
-console.log("User flipped" + " " + cardOne);
 
-//created a second variable for cardTwo to represent the card the suer flipped
-var cardTwo = cards[2];
-//added card to array of cards that in play
-cardsInPlay.push(cardTwo);
-//used console.log() to display the card that the user flipped
-console.log("User flipped" + " " + cardTwo);
-//check to see if two cards were played
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]){
-    alert("You found a match");
-  }
-  else {
-    alert("Sorry, try again.");
-  }
+//created a function to check for a match
+var checkForMatch = function() {
+  //check t see if two cards match and provide feedback to user
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
 }
+}
+
+//created a function to repeate a user flipping card
+var flipCard = function (cardId) {
+  //dislay the card the user just flipped
+console.log("User flipped" + cards[cardId]);
+//add card to array of cards that is in play
+cardsInPlay.push(cards[cardId]);
+  //check to see if two cards were played
+  if (cardsInPlay.length === 2) {
+    checkForMatch();
+  }
+};
+//for now, we are manually calling the flipCard function
+//to represent a users play
+//call the flipCard function, passing in an index as the argument
+flipCard(0);
+//call the flipCard function, as well.
+flipCard(2);
